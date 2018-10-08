bfb99e125610c1c7685f4d46307b7849c3a1017f
kehindemary <adebolakehindemary@gmail.com>
2018-10-08 16:25:46 +0100

    submission of mary task

---
+++ submissions/phptask/task4.php
@@ -0,0 +1,6 @@
+<?php
+for($a = 100; $a <= 150; $a++){
+    echo "the number is"." ".$a;
+    echo "<br>";
+}
+?>
---
+++ submissions/phptask/task5.php
@@ -0,0 +1,10 @@
+<?php
+$i = 2;
+do{
+    echo "the number is"." ".$i;
+    echo "<br>";
+    $i = $i + 2;
+}
+
+while($i <= 50);
+?>
